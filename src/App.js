f4fb168ea1824d002c1611bda0b37ce6a3852a62
Iryna Volosozharova <iradevis@gmail.com>
2021-07-25 16:05:51 +0300

    Added local storage saving

--- src/App.js
+++ src/App.js
@@ -18,6 +18,21 @@ class App extends Component {
     filter: "",
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = (data) => {
     const { contacts } = this.state;
     const allContacts = contacts.map((contact) => contact.name.toLowerCase());
